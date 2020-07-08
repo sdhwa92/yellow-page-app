ca5d6b58606a262a36a59fa5c1b6c9e84bd686eb
Daehwa Seo <sdhwa92@gmail.com>
2020-07-08 21:36:03 +1000

    Add layout components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,18 @@ import { SharedModule } from '@shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavComponent } from './layout/nav/nav.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthLayoutComponent,
+    ContentLayoutComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     // angular
