e095c8b01b02d02e3be1a70279d28253e6292829
Daehwa Seo <sdhwa92@gmail.com>
2020-07-27 20:13:30 +1000

    Change the app name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('home-mart-app app is running!');
+    expect(page.getTitleText()).toEqual('yellow-page-app app is running!');
   });
 
   afterEach(async () => {
